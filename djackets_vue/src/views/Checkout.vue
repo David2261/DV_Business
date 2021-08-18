a5b68eac7c4366a0aa32a5fcb2691eae78d14ff4
Bulat <65621634+David2261@users.noreply.github.com>
2021-08-18 22:50:18 +0300

    Update Checkout.vue
    Delete Token

--- djackets_vue/src/views/Checkout.vue
+++ djackets_vue/src/views/Checkout.vue
@@ -143,7 +143,7 @@ export default {
         this.cart = this.$store.state.cart
         // Добавляет товар в Stripe нужно добавить добавить токен
         if (this.cartTotalLength > 0) {
-            this.stripe = Stripe('pk_test_51H1HiuKBJV2qfWbD2gQe6aqanfw6Eyul5PO2KeOuSRlUMuaV4TxEtaQyzr9DbLITSZweL7XjK3p74swcGYrE2qEX00Hz7GmhMI')
+            this.stripe = Stripe('ADD_YOUR_STRIPE_TOKEN')
             const elements = this.stripe.elements();
             this.card = elements.create('card', { hidePostalCode: true })
             this.card.mount('#card-element')
